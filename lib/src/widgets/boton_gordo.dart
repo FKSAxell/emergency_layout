30ff55482c46734b3a3bc2900c071ff13d813599
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-03-30 12:07:27 -0500

    Información dentro del Botón

--- lib/src/widgets/boton_gordo.dart
+++ lib/src/widgets/boton_gordo.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BotonGordo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return _BotonGordoBackground();
+    return Stack(
+      children: [
+        _BotonGordoBackground(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 140,
+              width: 40,
+            ),
+            FaIcon(
+              FontAwesomeIcons.carCrash,
+              color: Colors.white,
+              size: 40,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Expanded(
+              child: Text(
+                'Motor Accident',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+            FaIcon(
+              FontAwesomeIcons.chevronRight,
+              color: Colors.white,
+            ),
+            SizedBox(
+              width: 40,
+            )
+          ],
+        ),
+      ],
+    );
   }
 }
 
@@ -11,6 +46,19 @@ class _BotonGordoBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Stack(
+          children: [
+            Positioned(
+              right: -20,
+              top: -20,
+              child: FaIcon(FontAwesomeIcons.carCrash,
+                  size: 150, color: Colors.white.withOpacity(0.2)),
+            ),
+          ],
+        ),
+      ),
       width: double.infinity,
       height: 100,
       margin: EdgeInsets.all(20),
