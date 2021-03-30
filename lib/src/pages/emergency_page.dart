e564bac945adfacdbe73d9f5115c78c2b99991d7
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-03-30 11:42:21 -0500

    Fondo del Botón Gordo

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,3 +1,4 @@
+import 'package:emergency_layout/src/widgets/boton_gordo.dart';
 import 'package:emergency_layout/src/widgets/headers.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -6,13 +7,26 @@ class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IconHeader(
-        icon: FontAwesomeIcons.plusCircle,
-        subTitulo: 'Haz Solicitado',
-        titulo: 'Asistencia Médica',
-        color1: Color(0xff526BF6),
-        color2: Color(0xff67ACF2),
+      body: Center(
+        child: BotonGordo(),
       ),
+    );
+  }
+}
+
+class PageHeader extends StatelessWidget {
+  const PageHeader({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconHeader(
+      icon: FontAwesomeIcons.plus,
+      subTitulo: 'Haz Solicitado',
+      titulo: 'Asistencia Médica',
+      color1: Color(0xff526BF6),
+      color2: Color(0xff67ACF2),
     );
   }
 }
