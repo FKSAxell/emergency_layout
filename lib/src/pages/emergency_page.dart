35361ae31398df553dce8eb79b5deb0e005457d7
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-03-30 11:30:48 -0500

    Parametrizando widget header: titulo,sub y icon

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,11 +1,18 @@
 import 'package:emergency_layout/src/widgets/headers.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IconHeader(),
+      body: IconHeader(
+        icon: FontAwesomeIcons.plusCircle,
+        subTitulo: 'Haz Solicitado',
+        titulo: 'Asistencia Médica',
+        color1: Color(0xff526BF6),
+        color2: Color(0xff67ACF2),
+      ),
     );
   }
 }
--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HeaderCuadrado extends StatelessWidget {
   @override
@@ -339,6 +340,71 @@ class _HeaderWaveGradientPainter extends CustomPainter {
 }
 
 class IconHeader extends StatelessWidget {
+  final IconData icon;
+  final String titulo;
+  final String subTitulo;
+  final Color color1;
+  final Color color2;
+
+  const IconHeader({
+    @required this.icon,
+    @required this.titulo,
+    @required this.subTitulo,
+    this.color1 = Colors.grey,
+    this.color2 = Colors.blueGrey,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final Color colorBlanco = Colors.white.withOpacity(0.7);
+
+    return Stack(
+      children: [
+        _IconHeaderBackground(
+          color1: this.color1,
+          color2: this.color2,
+        ),
+        Positioned(
+          top: -50,
+          left: -70,
+          child: FaIcon(this.icon,
+              size: 230, color: Colors.white.withOpacity(0.2)),
+        ),
+        Column(
+          children: [
+            SizedBox(height: 80, width: double.infinity),
+            Text(
+              this.subTitulo,
+              style: TextStyle(fontSize: 20, color: colorBlanco),
+            ),
+            SizedBox(height: 20),
+            Text(
+              this.titulo,
+              style: TextStyle(
+                fontSize: 25,
+                color: colorBlanco,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            FaIcon(this.icon, size: 80, color: Colors.white),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _IconHeaderBackground extends StatelessWidget {
+  final Color color1;
+  final Color color2;
+
+  const _IconHeaderBackground({
+    Key key,
+    @required this.color1,
+    @required this.color2,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -352,10 +418,7 @@ class IconHeader extends StatelessWidget {
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
-          colors: [
-            Color(0xff526bf6),
-            Color(0xff67acf2),
-          ],
+          colors: [this.color1, this.color2],
         ),
       ),
     );
